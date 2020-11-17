f5f57add81c5187047de581f9f7e4b124fc04443
Jacob Wennebro <jacob.wennebro@learnet.se>
2020-11-17 10:30:07 +0100

    first commit

---
+++ src/main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+
+public class main {
+
+    private static void evaluator(int guess, int num) {
+        if(num < guess) {
+            JOptionPane.showMessageDialog(null, "Gissningen är större än svaret.");
+        } else if(num > guess) {
+            JOptionPane.showMessageDialog(null, "Gissningen är mindre än svaret.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Du hade rätt!");
+            return;
+        }
+
+        makeGuess(num);
+    }
+
+    private static void makeGuess(int num) {
+        int guess = Integer.parseInt(JOptionPane.showInputDialog(null, "Gissa numret :)"));
+        evaluator(guess, num);
+    }
+
+    public static void main(String[] args) {
+        int num = (int) Math.round(Math.random()*100);
+
+        try {
+            makeGuess(num);
+        } catch(Exception e) {
+            JOptionPane.showInputDialog(null, "Svaret måste vara ett nummer!");
+            makeGuess(num);
+        }
+
+    }
+
+}
